ae67c537b3c21cc2872512c4231ec6fa7fe887b0
GeeraertHerber <herber.geeraert@student.howest.be>
2022-03-11 15:55:27 +0100

    ENH

--- daemon-console/Models/ApiCaller.cs
+++ daemon-console/Models/ApiCaller.cs
@@ -7,25 +7,49 @@ namespace daemon_console.Models
     internal class ApiCaller
     {
        
-        public static string GetSite(string SiteId = "")
+        public static string UrlCreator(string url, bool beta = false)
         {
-            return $"/sites/{SiteId}";
-        }
-
-        public static string GetDriveBySite(string SiteId)
-        {
-            return $"/sites/{SiteId}/drive";
-        }
-
-        public static string GetFilesByDrive(string DriveId, bool RootOnly = false)
-        {
-            if (!RootOnly)
-                return $"/drives/{DriveId}/root/children";
+            string standardUrl = "https://graph.microsoft.com/";
+            if (beta)
+            {
+                standardUrl += "beta/";
+            }
             else
             {
-                return $"";
+                standardUrl += "v1.0/";
             }
+            standardUrl += url;
+            return standardUrl;
+        }
+
+        public static string GetSite(string siteId = "root")
+        {
+            string url;
+            url = UrlCreator($"sites/{siteId}");
+            return url;
         }
         
+        public static string GetDriveBySite(string siteId, bool standard = true)
+        {
+            string url; 
+            if (standard)
+            {
+                //Gets the standard drive for this site
+                url = UrlCreator($"sites/{siteId}/drive");
+            }
+            else
+            {
+                //Gets all the drives connected to this site
+                url = UrlCreator($"sites/{siteId}/drives");
+            }
+            return url;
+        }
+
+        public static string GetFilesByDrive(string driveId, string pathRelative = "")
+        {
+            string url;
+            url = UrlCreator($"/drives/{driveId}/root:/{pathRelative}:/children");
+            return url;
+        }
     }
 }
