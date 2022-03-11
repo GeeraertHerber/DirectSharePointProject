83c81abb7b5b68b0204f6f48497dc413355f5c70
GeeraertHerber <herber.geeraert@student.howest.be>
2022-03-11 10:11:47 +0100

    ADD: errorhandeler + creator

---
+++ daemon-console/Models/Errors/ErrorHandler.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace daemon_console.Models.Errors
+{
+    internal class ErrorHandler
+    {
+        public static JObject CreateNewError(string errorMessage)
+        {
+            RootError error = new RootError
+            {
+                Error = new Error
+                {
+                    Code = errorMessage,
+                    Message = "Not good",
+                    InnerError = new InnerError
+                    {
+                        RequestId = Guid.NewGuid(),
+                        Date = DateTime.Now,
+                        ClientRequestId = Guid.NewGuid(),
+                        Code = errorMessage
+                    }
+                }
+            };
+            JObject errorJson = (JObject)JsonConvert.SerializeObject(error.ToString());
+            return errorJson;
+        }
+    }
+}
